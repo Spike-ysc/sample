42e903c84be3da895532dd13e96225b2e75dc26f
Gogs <gogs@fake.local>
2021-02-19 14:32:11 +0800

    增加豆瓣电影api

--- src/main/java/henu/yan/sample/repository/DoubanRepository.java
+++ src/main/java/henu/yan/sample/repository/DoubanRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 //实现RESTful跨域，RESTful跨域请求需要单独做处理
 @CrossOrigin
 public interface DoubanRepository extends JpaRepository<Douban, Integer> {
@@ -18,11 +20,21 @@ public interface DoubanRepository extends JpaRepository<Douban, Integer> {
     @RestResource(path = "id", rel = "id")
     Douban findByIdEquals(@Param("id") String id);
 
+//    /**
+//     * 查询douban表中name相同的豆瓣电影
+//     * @param name 要查询的电影名
+//     * @return  匹配到的电影
+//     */
+//    @RestResource(path = "name", rel = "name")
+//    Douban findByNameEquals(@Param("name") String name);
+
     /**
-     * 查询douban表中name相同的豆瓣电影
-     * @param name 要查询的电影名
-     * @return  匹配到的电影
+     * 模糊查询符合name的电影名
+     * @param name 电影关键字 模糊查询
+     * @return  含有电影关键字的电影
      */
     @RestResource(path = "name", rel = "name")
-    Douban findByNameEquals(@Param("name") String name);
+    List<Douban> findByNameContaining(String name);
+
+
 }

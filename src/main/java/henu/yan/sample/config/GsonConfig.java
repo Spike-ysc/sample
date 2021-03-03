aedcc5293ac5f6207cfa661a2a963bcaa7b285e3
Gogs <gogs@fake.local>
2021-03-03 19:36:17 +0800

    添加项目监控Admin

--- src/main/java/henu/yan/sample/config/GsonConfig.java
+++ src/main/java/henu/yan/sample/config/GsonConfig.java
@@ -1,11 +1,6 @@
 package henu.yan.sample.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import java.lang.reflect.Modifier;
 
 /**
  * Gson自定义类
@@ -15,14 +10,14 @@ import java.lang.reflect.Modifier;
 public class GsonConfig {
 
     //处理Gson返回日期的格式
-    @Bean
-    GsonHttpMessageConverter gsonHttpMessageConverter(){
-        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
-        GsonBuilder builder = new GsonBuilder();
-        builder.setDateFormat("yyyy-MM-dd");
-        builder.excludeFieldsWithModifiers(Modifier.PROTECTED);
-        Gson gson = builder.create();
-        converter.setGson(gson);
-        return converter;
-    }
+//    @Bean
+//    GsonHttpMessageConverter gsonHttpMessageConverter(){
+//        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+//        GsonBuilder builder = new GsonBuilder();
+//        builder.setDateFormat("yyyy-MM-dd");
+//        builder.excludeFieldsWithModifiers(Modifier.PROTECTED);
+//        Gson gson = builder.create();
+//        converter.setGson(gson);
+//        return converter;
+//    }
 }
